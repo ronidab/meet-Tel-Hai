e45ca25c97337bb854e3ce89321b076844282238
HadasahS <62838748+HadasahS@users.noreply.github.com>
2021-09-27 22:30:45 +0300

    fix addExpense

--- src/components/addExpense.jsx
+++ src/components/addExpense.jsx
@@ -3,96 +3,93 @@ import { AuthContext } from "./../contexts/auth";
 import splitBillService from "../services/SplitBillService";
 
 class AddExpence extends Component {
-	static contextType = AuthContext;
+  static contextType = AuthContext;
 
-	componentDidMount() {
-		console.log(this.props)
-	}
-	state = {
-		title: "",
-		sum: 0,
-		category: "",
-		date: 0,
-	};
+  componentDidMount() {}
+  state = {
+    title: "",
+    sum: 0,
+    category: "",
+    date: 0,
+  };
 
-	handleChange = (e) => {
-		const key = e.currentTarget.name;
-		const value = e.currentTarget.value;
-		this.setState({ [key]: value });
-	};
+  handleChange = (e) => {
+    const key = e.currentTarget.name;
+    const value = e.currentTarget.value;
+    this.setState({ [key]: value });
+  };
 
-	handleSubmit = async (e) => {
-		e.preventDefault();
-		const { groupId } = this.props;
-		try {
-			await splitBillService.addExpence(groupId, this.state);
-		} catch (err) {
-			this.setState({ errors: { page: "wrong email/password" } });
-			console.log(err);
-			return; //handle error
-		}
-	};
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const { groupId } = this.props;
+    try {
+      await splitBillService.addExpense(groupId, this.state);
+    } catch (err) {
+      this.setState({ errors: { page: "wrong email/password" } });
+      console.log(err);
+      return; //handle error
+    }
+  };
 
-	render() {
-		console.log(this.state);
-		return (
-			<form
-				className="border border-dark bg-info p-3"
-				onSubmit={this.handleSubmit}
-			>
-				<div className="form-inline">
-					<div className="form-control-md p-2">
-						<input
-							type="text"
-							className=" -control"
-							placeholder="Title"
-							value={this.state.title}
-							onChange={this.handleChange}
-							id="title"
-							name="title"
-						/>
-					</div>
-					<div className="form-control-md p-2">
-						<input
-							type="text"
-							className="Sum"
-							placeholder="$"
-							value={this.state.sum}
-							onChange={this.handleChange}
-							id="sum"
-							name="sum"
-						/>
-					</div>
-					<div className="form-control-md p-2">
-						<select
-							className="custom-select my-1 mr-sm-2"
-							id="inlineFormCustomSelectPref"
-							value={this.state.category}
-							onChange={this.handleChange}
-							name="category"
-						>
-							<option>Category...</option>
-							<option>Grocerirs</option>
-							<option>Bills</option>
-							<option>Others</option>
-						</select>
-					</div>
-					<div className="form-control-md p-2">
-						<input
-							type="date"
-							className="form-control"
-							value={this.state.date}
-							onChange={this.handleChange}
-							name="date"
-						/>
-					</div>
-					<div className="form-group p-2 ">
-						<button className="btn  btn-secondary -sm">Add expense</button>
-					</div>
-				</div>
-			</form>
-		);
-	}
+  render() {
+    return (
+      <form
+        className="border border-dark bg-info p-3"
+        onSubmit={this.handleSubmit}
+      >
+        <div className="form-inline">
+          <div className="form-control-md p-2">
+            <input
+              type="text"
+              className=" -control"
+              placeholder="Title"
+              value={this.state.title}
+              onChange={this.handleChange}
+              id="title"
+              name="title"
+            />
+          </div>
+          <div className="form-control-md p-2">
+            <input
+              type="text"
+              className="Sum"
+              placeholder="$"
+              value={this.state.sum}
+              onChange={this.handleChange}
+              id="sum"
+              name="sum"
+            />
+          </div>
+          <div className="form-control-md p-2">
+            <select
+              className="custom-select my-1 mr-sm-2"
+              id="inlineFormCustomSelectPref"
+              value={this.state.category}
+              onChange={this.handleChange}
+              name="category"
+            >
+              <option>Category...</option>
+              <option>Grocerirs</option>
+              <option>Bills</option>
+              <option>Others</option>
+            </select>
+          </div>
+          <div className="form-control-md p-2">
+            <input
+              type="date"
+              className="form-control"
+              value={this.state.date}
+              onChange={this.handleChange}
+              name="date"
+            />
+          </div>
+          <div className="form-group p-2 ">
+            <button className="btn  btn-secondary -sm">Add expense</button>
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default AddExpence;
--- src/services/SplitBillService.js
+++ src/services/SplitBillService.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 class SplitBillService {
-	allGroups = () => axios.get("/groups");
-	groupById = (id) => axios.get(`/groups/${id}`);
-	addGroup = ({ name }) => axios.post("/groups", { name });
+  allGroups = () => axios.get("/groups");
+  groupById = (id) => axios.get(`/groups/${id}`);
+  addGroup = ({ name }) => axios.post("/groups", { name });
 
-	addExpence = (groupId, { title, sum, category, date }) => {
-		return axios.post(`/groups/${groupId}`, { title, sum, category, date });
-	}
+  addExpense = (groupId, { title, sum, category, date }) => {
+    return axios.post(`/groups/${groupId}`, { title, sum, category, date });
+  };
 }
 
 export default new SplitBillService();
