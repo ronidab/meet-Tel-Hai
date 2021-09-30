849b6b26142197e95baeff65bb7e08d484a41d18
Abziz <Abziz89@gmail.com>
2021-09-30 18:13:50 +0300

    fix production condition

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ const authRouter = require("./routes/auth");
 const path = require('path');
 const jwtMiddleware = require("./middleware/jwt");
 
+const PORT = process.env.PORT || 3001;
+const PRODUCTION = process.env.NODE_ENV === 'production'
 
 mongoose
 	.connect(process.env.DB_URI, {
@@ -17,20 +19,17 @@ mongoose
 	})
 	.then(() => console.log("connected to DB"));
 
-const port = process.env.PORT || 3001;
 
 const app = express();
-
-// if (process.env.NODE_ENV === 'production') {
-// 	app.use(express.static('/build'));
-// }
-app.use(express.static('build'));
+if (PRODUCTION) {
+	app.use(express.static('/build'));
+}
 app.use(express.json());
 app.use('/api/auth', authRouter);
 app.use('/api/groups', jwtMiddleware, groupRouter);
-app.get('/*', (req, res) => {
-	res.sendFile(path.resolve('build/index.html'));
-})
-
-// http://localhost:3000/groups 
-app.listen(port);
+if (PRODUCTION) {
+	app.get('/*', (req, res) => {
+		res.sendFile(path.resolve('build/index.html'));
+	})
+}
+app.listen(PORT);
