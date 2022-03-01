408ff50a9cdee602006710c24d6c044899f2ecf6
Roni Dabah <roni148148@gmail.com>
2022-03-01 15:53:38 +0200

    change homepage

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,13 +7,13 @@ class Homse extends Component {
   render() {
     console.log(this.context);
     return (
-      <div className="row h-100 bg-info">
+      <div className="row h-100 bg-danger">
         <div className="col p-3 container">
           <h1 className="text-dark text-center">MeetTelHai</h1>
           <h5 className="text-center">
             {/* <pre>{JSON.stringify(this.context.currentUser, null, 2)}</pre> */}
-            Create your commune, call your friends, enter your expenses and we will
-            do the rest for you
+            Find your match, Create relationships in Tel-Hai community 
+            <nl></nl>log in and we will do the rest for you
           </h5>
         </div>
       </div>
