9a7e74e936107f19cd090fe28cf4ff9ced9c94eb
Roni Dabah <roni148148@gmail.com>
2022-03-01 16:13:42 +0200

    homepage pic2

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -12,9 +12,10 @@ class Homse extends Component {
           <h1 className="text-dark text-center">MeetTelHai</h1>
           <h5 className="text-center">
             {/* <pre>{JSON.stringify(this.context.currentUser, null, 2)}</pre> */}
-            Find your match, Create relationships in Tel-Hai community 
-            <nl></nl>log in and we will do the rest for you
-            <img src="/homepagepic.jpg" class="img-fluid" alt="Responsive image"></img>
+            Find your match, <br></br>
+            Create relationships in Tel-Hai community <br></br>
+            <br></br>log in and we will do the rest for you<br></br>
+            <img src="homepagepic.jpg" alt="Love img"></img>
           </h5>
         </div>
       </div>
