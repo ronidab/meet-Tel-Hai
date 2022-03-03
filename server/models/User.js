4e4fc35c2ed8b3584f8b9655efbfed5ed27e0242
Roni Dabah <roni148148@gmail.com>
2022-03-03 10:44:46 +0200

    int to number

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
 	name: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
-	age: {type: Int, required: true, unique:true},
+	age: {type: Number, required: true, unique:true},
 	gender: {type: String, required: true, unique: true},
 	attract_to: {type: String, required: true, unique: false},
 	password: { type: String, required: true },
